1ad77af90acefad76dd365669ecdc103fb4e5933
Andr3sGA <155799676+Andr3sGA@users.noreply.github.com>
2024-01-06 12:31:39 -0500

    Change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+    alert('Por eso te amo guapa <3')
 });
 
 const noBtn = document.querySelector('#noBtn');
